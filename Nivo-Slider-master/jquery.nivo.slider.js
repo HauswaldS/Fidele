95f50078a8f9fbf4b9c176e6162f997bf698fe97
HauswaldS <lbaxel95@gmail.com>
2016-05-19 12:34:49 +0200

    Ajout contact.html

--- Nivo-Slider-master/jquery.nivo.slider.js
+++ Nivo-Slider-master/jquery.nivo.slider.js
@@ -643,7 +643,7 @@
         pauseTime: 3000,
         startSlide: 0,
         directionNav: true,
-        controlNav: true,
+        controlNav: false,
         controlNavThumbs: false,
         pauseOnHover: true,
         manualAdvance: false,
